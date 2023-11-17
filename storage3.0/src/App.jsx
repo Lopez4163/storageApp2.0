966de44ad424e5c501c3dfb63735ce3b4df156c0
Nicholas Lopez <Lopez4163@gmail.com>
2023-11-17 15:17:05 -0600

    added poweredby

--- storage3.0/src/App.jsx
+++ storage3.0/src/App.jsx
@@ -1,48 +1,52 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import InventoryForm from "./iventoryForm";
 import InventoryTable from "./inventoryTable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
 import "./app.css";
 
 function App() {
-  const [inventoryItems, setInventoryItems] = useState([
-    { id: 1, itemName: "Default Item 1", quantity: 10, location: "Warehouse A", pricePerUnit: 5.0 },
-    { id: 2, itemName: "Default Item 2", quantity: 20, location: "Warehouse B", pricePerUnit: 8.0 },
-    // Add more default items as needed
-  ]);
+    const [inventoryItems, setInventoryItems] = useState([
+        {id: 1, itemName: "Default Item 1", quantity: 10, location: "Warehouse A", pricePerUnit: 5.0},
+        {id: 2, itemName: "Default Item 2", quantity: 20, location: "Warehouse B", pricePerUnit: 8.0},
+        // Add more default items as needed
+    ]);
 
-  const addItem = newItem => {
-    setInventoryItems([...inventoryItems, { ...newItem, id: Date.now() }]);
-  }
+    const addItem = newItem => {
+        setInventoryItems([...inventoryItems, {...newItem, id: Date.now()}]);
+    }
 
-  const deleteItem = index => {
-    const updatedItems = [...inventoryItems];
-    updatedItems.splice(index, 1);
-    setInventoryItems(updatedItems);
-  }
+    const deleteItem = index => {
+        const updatedItems = [...inventoryItems];
+        updatedItems.splice(index, 1);
+        setInventoryItems(updatedItems);
+    }
 
-  const editItem = (index, updatedItem) => {
-    const updatedItems = [...inventoryItems];
-    updatedItems[index] = updatedItem;
-    setInventoryItems(updatedItems);
-  }
+    const editItem = (index, updatedItem) => {
+        const updatedItems = [...inventoryItems];
+        updatedItems[index] = updatedItem;
+        setInventoryItems(updatedItems);
+    }
 
-  return (
+    return (
 
-      <div className="container">
+        <div className="container">
+            <div className="powered-by">
+                Powered By: <FontAwesomeIcon icon={faReact} className="icon" />
+            </div>
+            <div className="form-container">
+                <InventoryForm addItem={addItem}/>
+            </div>
+            <div className="table-container">
+                <InventoryTable
+                    items={inventoryItems}
+                    deleteItem={deleteItem}
+                    editItem={editItem}
+                />
 
-        <div className="form-container">
-          <InventoryForm addItem={addItem} />
+            </div>
         </div>
-        <div className="table-container">
-          <InventoryTable
-              items={inventoryItems}
-              deleteItem={deleteItem}
-              editItem={editItem}
-          />
-
-        </div>
-      </div>
-  );
+    );
 }
 
 export default App;
